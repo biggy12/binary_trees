8bee3d65b41aabd466de939a0ae918bce0369124
Eze Favour Iheanyichukwu <39442574+biggy12@users.noreply.github.com>
2024-02-26 11:17:48 +0100

    Create 3-binary_tree_delete.c

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root node of the tree to delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+    if (tree == NULL)
+    {
+        return;
+    }
+
+    /* Delete left and right subtrees recursively */
+    binary_tree_delete(tree->left);
+    binary_tree_delete(tree->right);
+
+    /* Free the current node */
+    free(tree);
+}
