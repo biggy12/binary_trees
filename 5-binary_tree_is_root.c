2f80c3857e1f2cbba5adda51fd3dbdba1ff5fdf1
Eze Favour Iheanyichukwu <39442574+biggy12@users.noreply.github.com>
2024-02-26 11:20:27 +0100

    Create 5-binary_tree_is_root.c

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,10 @@
+/**
+ * binary_tree_is_root - Checks if a node is a root
+ * @node: Pointer to the node to check
+ *
+ * Return: 1 if the node is a root, 0 otherwise
+ */
+int binary_tree_is_root(const binary_tree_t *node)
+{
+    return node != NULL && node->parent == NULL;
+}
